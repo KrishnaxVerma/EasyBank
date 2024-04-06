2f4f2474d5df5160ce55e37088e767b5ef80a314
KrishnaxVerma <161187375+KrishnaxVerma@users.noreply.github.com>
2024-04-06 19:32:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ document.getElementById('navButton').addEventListener('click', function() {
       navOptions.classList.add('hidden');
    }
   });
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var animatedElement = document.querySelector('.animated-element');
+  
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      );
+    }
+  
+    function handleScroll() {
+      if (isElementInViewport(animatedElement)) {
+        animatedElement.classList.add('animate');
+        // Remove the event listener after the animation has been triggered
+        window.removeEventListener('scroll', handleScroll);
+      }
+    }
+  
+    // Initial check in case the element is already in viewport
+    handleScroll();
+  
+    // Listen for scroll events
+    window.addEventListener('scroll', handleScroll);
+  });
+
+ 
